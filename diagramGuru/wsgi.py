d3b9c6a73905ddf670aace05ce50c3fed1e95b3a
cdchushig <cdavid.chushig@gmail.com>
2021-02-09 14:25:17 +0100

    [cdchushig] Added files of Django project

---
+++ diagramGuru/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for diagramGuru project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diagramGuru.settings')
+
+application = get_wsgi_application()
