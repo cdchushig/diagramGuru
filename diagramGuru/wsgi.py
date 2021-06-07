b5d1b3e5b617a77cf738c8e7434b53aa7ee30e5c
cdchushig <cdavid.chushig@gmail.com>
2021-06-07 05:10:49 +0200

    Updated wsgi.py

--- diagramGuru/wsgi.py
+++ diagramGuru/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diagramGuru.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diagramGuru.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'diagramGuru.settings'
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+import django
+django.setup()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
