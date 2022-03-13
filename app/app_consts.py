6dd0b3d19025002972e951651cf189e17360af62
cdchushig <cdavid.chushig@gmail.com>
2022-03-13 09:51:47 +0100

    Modified port of api

--- app/app_consts.py
+++ app/app_consts.py
@@ -2,7 +2,7 @@ class Consts(object):
     """
     Class used to unify the different constant values used in entire project
     """
-    API_URL = 'http://127.0.0.1:8080/api/v1/detect'
+    API_URL = 'http://127.0.0.1:5000/api/v1/detect'
     ACTOR = "actor"
     OBJECT = "object"
     DECISION = "decision"
